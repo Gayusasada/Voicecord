9a5cb1bbd6af6ad7eb94062e339e0d21d5d2ed09
Gayusasada <130235223+Gayusasada@users.noreply.github.com>
2024-01-15 01:17:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ from keep_alive import keep_alive
 
 status = "online" #online/dnd/idle
 
-GUILD_ID = ADD_YOUR_SERVER_ID_HERE
-CHANNEL_ID = ADD_YOUR_CHANNEL_ID_HERE
+GUILD_ID = 1188813133075185674
+CHANNEL_ID = 1193871626018770994
 SELF_MUTE = True
-SELF_DEAF = False
+SELF_DEAF = True
 
 usertoken = os.getenv("TOKEN")
 if not usertoken:
